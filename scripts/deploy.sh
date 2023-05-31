4a876c6711b37859e4252af24351cee1eb62ab36
Hyeonkyoon <30308499+hyeonkyoon@users.noreply.github.com>
2023-05-31 14:52:51 +0900

    Update deploy.sh
    Update start script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-REPOSITORY=/home/ec2-user/app/step2
+REPOSITORY=/home/ubuntu
 PROJECT_NAME=freelec-springboot2-webservice
 
 echo "> Build 파일 복사"
 
-cp $REPOSITORY/zip/*.jar $REPOSITORY/
+cp $REPOSITORY/deploy_zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
@@ -34,6 +34,5 @@ chmod +x $JAR_NAME
 echo "> $JAR_NAME 실행"
 
 nohup java -jar \
-    -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
-    -Dspring.profiles.active=real \
+    -Dspring.config.location=classpath:/application.properties,/home/ubuntu/app/application-oauth.properties \
     $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
